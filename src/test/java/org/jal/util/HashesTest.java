bfc955d1299334cd7a8fcc790b913e8858b404f4
W. Cho <wcho.dev@gmail.com>
2024-07-05 14:05:00 +0900

    rearrange hash test cases

--- src/test/java/org/jal/util/HashesTest.java
+++ src/test/java/org/jal/util/HashesTest.java
@@ -3,19 +3,16 @@ package org.jal.util;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 
 public class HashesTest {
   @DisplayName("multiplyShiftHash() should be less than the bound")
-  @Test
-  public void testSwap() {
-    int bound = 100;
+  @CsvSource({"1", "10", "100", "1000000000"})
+  @ParameterizedTest(name = "{0}")
+  public void testSwap(int bound) {
 
-    for (long i = 0; i < 10; ++i) {
-      assertTrue(Hashes.multShiftHash(i, bound) < bound);
-    }
-    for (long i = 0x7FFFFFF0; i < 0x7FFFFFFF; ++i) {
-      assertTrue(Hashes.multShiftHash(i, bound) < bound);
-    }
+    assertTrue(Hashes.multShiftHash(0, bound) < bound);
+    assertTrue(Hashes.multShiftHash(bound-1, bound) < bound);
   }
 }
